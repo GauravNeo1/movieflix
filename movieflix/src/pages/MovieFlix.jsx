47010b79ba71fb7dda9725fbb9bc34009838c071
Gaurav Patil <patil.gaurav@neosoftmail.com>
2024-09-16 15:05:15 +0530

    feat: add movieflix logo
    fix: hide genre filter scrollbar

--- movieflix/src/pages/MovieFlix.jsx
+++ movieflix/src/pages/MovieFlix.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import MovieList from "../components/MovieFlix/MovieList";
 import GenreFilter from "../components/MovieFlix/GenreFilter";
+import "../styles/MovieFlix.css";
 
 const MovieFlix = () => {
   const [selectedGenres, setSelectedGenres] = useState([]);
   
   return (
     <div>
-      <h1>MOVIEFLIX</h1>
+      <div class="logo-container">
+        <img src='/logo.png' alt="Logo" />
+      </div>
 
       <GenreFilter
         onGenreChange={(selectedGenres) => {
